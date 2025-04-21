2e849a60a068ca50009aa5e563efb8a9c7909877
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-04-21 20:28:10 -0300

    Arrumando estrutura dos arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,14 @@ function App() {
                     <span class="text-3xl font-bold">Nome do Site</span>
                     <div class="font-sans flex space-x-3">
                         <a href="#sobre">Sobre</a>
-                        <a href="#contato">Contato</a>
+                        <a href="#contato">Entrar em Contato</a>
                         <a href="#adotar">Adotar</a>
                         <a href="#cadastro">Entrar</a>
                         <a href="#cadastro">Cadastro</a>
                     </div>
                 </h1>
             </header>
+            <section class="flex justify-center items-center"></section>
             <footer class="border-t-1 border-b-1 border-zinc-200 dark:border-zinc-700 text-center text">
                 <h1 class="text-gray-700 dark:text-gray-400 font-sans">
                     © 2025 Nome do Site. Todos os direitos reservados.
