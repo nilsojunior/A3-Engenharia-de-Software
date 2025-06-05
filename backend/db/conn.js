0dcb3fdf38d0e1d07b090cdd1440dc4fa491cdf5
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-06-05 14:59:49 -0300

    Conectando com mongodb

---
+++ backend/db/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/adoteme')
+    console.log("banco de dados conectado")
+}
+
+main().catch((err) => console.log(err))
+
+module.exports = mongoose
