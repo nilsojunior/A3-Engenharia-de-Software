743ea7ace2a9c05dca3abc5e4fbd95ce6635c666
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-06-05 14:47:11 -0300

    Reestruturando backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+
+const UserRoutes = require('./routes/UserRoutes')
+const app = express()
+
+//JSON config
+app.use(express.json())
+
+
+//CORS Solucao
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+
+
+// Pasta Publica de imagens
+app.use(express.static('public'))
+
+// Rotas
+app.use('/users', UserRoutes)
+
+app.listen(5000)
