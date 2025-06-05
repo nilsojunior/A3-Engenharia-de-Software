57084de1335d048a1647da8cde30e5a8356346ae
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-06-05 15:01:12 -0300

    Create helpers
    - Criar e retornar tokens

---
+++ backend/helpers/create-user-token.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken")
+
+const createUserToken = async (user, req, res) => {
+
+    // Criando token
+
+    const token = jwt.sign({
+        name: user.name,
+        id: user.id
+    }, 'secret',
+    )
+
+    // Return Token
+
+    res.status(200).json({
+        message: "Você foi autenticado com sucesso!",
+        token: token,
+        userId: user._id,
+    })
+
+}
+
+module.exports = createUserToken
