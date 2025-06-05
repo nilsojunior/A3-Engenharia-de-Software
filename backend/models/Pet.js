9219a1e30cda98f46b305d184df6bc02e11a98e5
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-06-05 15:02:54 -0300

    Create models
    - Criar modelo pet
    - Criar modelo user

---
+++ backend/models/Pet.js
@@ -0,0 +1,51 @@
+const { model } = require('mongoose')
+const mongoose = require('../db/conn')
+const { Schema } = mongoose
+
+const Pet = mongoose.model(
+    'Pet',
+    new Schema(
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            age: {
+                type: Number,
+                required: true
+            },
+            weight:{
+                type: Number,
+                required: true
+            },
+            breed: {
+                type: String,
+                required: true
+            },
+            gender: {
+                type: String,
+                required: true
+            },
+            size: {
+                type: String,
+                required: true
+            },
+            local: {
+                type: String,
+                required: true
+            },
+            images: {
+                type: Array,
+                required: true
+            },
+            available: {
+                type: Boolean
+            },
+            user: Object,
+            adopter: Object,
+        },
+        { timestamps: true },
+    ),
+)
+
+module.exports = Pet
