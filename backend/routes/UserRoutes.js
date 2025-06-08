b3dec20352e175660a51cfe3ce505462b34a252a
Nilso Junior <162613094+Nilsojunior@users.noreply.github.com>
2025-06-08 17:08:04 -0300

    Update UserRoutes.js
    - Novas rotas.

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,9 +1,24 @@
 const router = require("express").Router();
 const UserController = require("../controllers/UserController");
 
+// Middleware
+const verifyToken = require("../helpers/verify-token");
+const { imageUpload } = require("../helpers/image-upload");
+
+// Rotas para POST
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+
+// Rotas para GET
 router.get("/checkuser", UserController.checkUser);
+router.get("/:id", UserController.getUserById);
+
+// Rotas para PATCH
+router.patch(
+    "/edit/:id",
+    verifyToken,
+    imageUpload.single("image"),
+    UserController.editUser,
+);
 
 module.exports = router;
-
